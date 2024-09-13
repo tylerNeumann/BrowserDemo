7ec3c9b8d68facf34f28c54ad06cf9d48be36a1e
tylerNeumann <124943406+tylerNeumann@users.noreply.github.com>
2024-09-13 13:13:54 -0500

    browser demo complete

--- app/src/main/java/fvtc/edu/browserdemo/MainActivity.java
+++ app/src/main/java/fvtc/edu/browserdemo/MainActivity.java
@@ -1,9 +1,14 @@
 package fvtc.edu.browserdemo;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -15,6 +20,8 @@ import fvtc.edu.browserdemo.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private WebView webView;
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,34 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        navView.setOnItemSelectedListener(onItemSelectedListener);
+        webView = findViewById(R.id.webView);
+
     }
 
+    private NavigationBarView.OnItemSelectedListener onItemSelectedListener = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            int id = item.getItemId();
+                if(id == R.id.navigation_dashboard) {
+                    webView.setWebViewClient(new WebViewClient());
+                    webView.loadUrl("https://foote.azurewebsites.net/Home");
+                }
+                else if(id == R.id.navigation_notifications){
+
+                    String html = "<html><body style='-color: cornflowerblue;'>";
+                    html += "<table border='1'>";
+                    html += "<tr><td>Fred</td></tr>";
+                    html += "<tr><td>Barney</td></tr>";
+                    html += "<tr><td>Wilma</td></tr>";
+                    html += "<tr><td>Betty</td></tr>";
+                    html += "</table></body></html>";
+
+                    webView.loadData(html, "text/html", "UTF-8");
+
+            }
+            return true;
+        }
+    };
 }
